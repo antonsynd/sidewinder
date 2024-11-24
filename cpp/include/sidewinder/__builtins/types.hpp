724c6402f8d8284c03a9339401efdd88daa282b4
antonsynd <nguyen.antony@gmail.com>
2024-11-23 23:29:33 -0500

    types: Add more type aliases

--- cpp/include/sidewinder/__builtins/types.hpp
+++ cpp/include/sidewinder/__builtins/types.hpp
@@ -1,14 +1,25 @@
+#include <cstddef>
+#include <memory>
+#include <optional>
 #include <type_traits>
+
+using Int = int;
+using Bool = bool;
+using Float = float;
 
 struct Complex {
  public:
-  int Real() const;
-  int Imag() const;
+  Int Real() const { return real_; }
+  Int Imag() const { return imag_; }
+
+ private:
+  Int real_;
+  Int imag_;
 };
 
 template <typename T>
-concept Value = std::is_same_v<T, int> || std::is_same_v<T, float> ||
-                std::is_same_v<T, bool> || std::is_same_v<T, Complex>;
+concept Value = std::is_same_v<T, Int> || std::is_same_v<T, Float> ||
+                std::is_same_v<T, Bool> || std::is_same_v<T, Complex>;
 
 template <typename T>
 concept Object = !Value<T>;
